97f3e365a427e461f5f90f93895a1f3be2d825e2
Dylan Lu <dyl055a@gmail.com>
2017-02-03 21:47:55 -0800

    cleaned up some stuff

--- src/ANN/ANN.py
+++ src/ANN/ANN.py
@@ -1,23 +1,29 @@
 import numpy as np
-import random
-import warnings
 import mnist_loader
-import codecs, json, os
+import codecs, json, os, warnings
+import time
 
 warnings.filterwarnings("ignore")
 
-#Neural Network using backpropogation and stochastic gradient descent
-def sigmoid(z):
-    return 1/(1+np.exp(-z))
-def sigmoid_prime(z):
-    return np.exp(-z)/((1+np.exp(-z))**2)
+# Neural Network using backpropogation and stochastic gradient descent
 
-def squared_error(y, yHat):
-    J = 0.5*sum((y-yHat)**2)
-    return J
+
+def sigmoid(z):
+    return 1/(1 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    return np.exp(-z)/((1 + np.exp(-z)) ** 2)
+
+
+def squared_error(y, y_hat):
+    j = 0.5 * sum((y - y_hat) ** 2)
+    return j
+
 
 class ANN(object):
-    def __init__(self, layer_sizes, regularization_function, learning_rate, bias_learning_rate, momentum=0, read_weights_from_file = False):
+    def __init__(self, layer_sizes, regularization_function, learning_rate, bias_learning_rate,
+                 momentum=0.0, read_weights_from_file=False):
         self.regularization_function = regularization_function
         self.activation_function = sigmoid
         self.activation_function_prime = sigmoid_prime
@@ -27,21 +33,16 @@ class ANN(object):
         self.momentum = momentum
         self.length = len(layer_sizes)
 
-
-        self.b = []
-        self.W = []
+        self.b = []  # Biases
+        self.W = []  # Weights
         self.delta_W = []
-        if read_weights_from_file == False:
+        if not read_weights_from_file:
             for i in range(self.length - 1):
                 weight = np.random.randn(self.layer_sizes[i], self.layer_sizes[i+1])
                 bias = np.random.randn(self.layer_sizes[i+1])
                 self.W.append(weight)
                 self.b.append(bias)
                 self.delta_W.append(np.zeros_like(weight))
-
-            self.W = np.asarray(self.W)
-            self.b = np.asarray(self.b)
-            self.delta_W = np.asarray(self.delta_W)
         else:
             for file_path in sorted(os.listdir("Optimal_Weights/Weights/")):
                 json_file = codecs.open("Optimal_Weights/Weights/" + file_path, 'r', encoding='utf-8').read()
@@ -49,7 +50,9 @@ class ANN(object):
                 weights = np.array(json_weights)
                 self.W.append(weights)
 
-
+        self.a = None  # Activations of layers
+        self.z = None  # Weights multiplied by input/activations
+        self.y_hat = None  # Output of neural net
 
     def forward(self, x):
         self.a = []
@@ -57,24 +60,25 @@ class ANN(object):
         self.z.append(np.add(np.dot(x, self.W[0]), self.b[0]))
         self.a.append(self.activation_function(self.z[0]))
         for weight in range(1, len(self.W)):
-            self.z.append(np.add(np.dot(self.a[weight-1], self.W[weight]),self.b[weight]))
+            self.z.append(np.add(np.dot(self.a[weight-1], self.W[weight]), self.b[weight]))
             self.a.append(self.activation_function(self.z[weight]))
-        return self.a[len(self.W)-1]
+        return self.a[-1]
 
     def loss_function_prime(self, x, y):
-        self.yHat = self.forward(x)
+        self.y_hat = self.forward(x)
         dJdWList = []
         dJdBList = []
 
-        delta = np.multiply(-(y-self.yHat), self.activation_function_prime(self.z[self.length-2]))
+        delta = np.multiply(-(y - self.y_hat), self.activation_function_prime(self.z[self.length - 2]))
         dJdBList.append(delta)
         dJdW = np.multiply(self.a[self.length-3][np.newaxis].T, delta[np.newaxis])
         dJdWList.append(dJdW)
-        for i in range(self.length-3, 0,-1):
+        for i in range(self.length-3, 0, -1):
             delta = np.dot(delta, self.W[i+1].T) * self.activation_function_prime(self.z[i])
             dJdBList.append(delta)
-            dJdW = np.multiply(np.asarray(self.a[i-1])[np.newaxis].T, np.asarray(delta))
-            dJdWList.append(dJdW)
+            dJdW = np.multiply(self.a[i-1][np.newaxis].T, delta)
+
+        dJdWList.append(dJdW)
         delta = np.dot(delta, self.W[1].T) * self.activation_function_prime(self.z[0])
         dJdBList.append(delta)
         xarray = np.array([x])
@@ -85,20 +89,20 @@ class ANN(object):
         dJdBList.reverse()
         return dJdWList, dJdBList
 
-    def back_prop(self, x,y):
+    def back_prop(self, x, y):
         dJdWList, dJdBList = self.loss_function_prime(x,y)
         if self.momentum:
             for i in range(len(dJdWList)):
-                currentWeight = self.W[i]
-                self.W[i] = self.W[i] - dJdWList[i] * self.learning_rate + self.momentum * self.delta_W[i]
-                self.delta_W[i] = self.W[i] - currentWeight
+                current_weight = self.W[i]
+                self.W[i] = self.W[i] - (dJdWList[i] * self.learning_rate) + (self.momentum * self.delta_W[i])
+                self.delta_W[i] = self.W[i] - current_weight
+                self.b[i] = self.b[i] - (dJdBList[i] * self.bias_learning_rate)
         else:
             for i in range(len(dJdWList)):
                 self.W[i] = self.W[i] - (dJdWList[i] * self.learning_rate)
                 self.b[i] = self.b[i] - (dJdBList[i] * self.bias_learning_rate)
         yh = self.forward(x)
         return squared_error(y, yh)
-
 
     def get_params(self):
         params = np.concatenate((self.W[0].ravel(), self.W[1].ravel()))
@@ -110,16 +114,15 @@ class ANN(object):
         w_start = 0
         w1_end = self.layer_sizes[0] * self.layer_sizes[1]
         self.W[0] = np.reshape(params[w_start:w1_end], (self.layer_sizes[0], self.layer_sizes[1]))
-
         for layer in range(1, self.length-1):
             w2_end = w1_end + self.layer_sizes[layer] * self.layer_sizes[layer+1]
             self.W[layer] = np.reshape(params[w1_end:w2_end], (self.layer_sizes[layer], self.layer_sizes[layer+1]))
             w1_end = w2_end
 
     def compute_gradients(self, x, y):
-        alldJdW = self.loss_function_prime(x,y)
+        alldJdW = self.loss_function_prime(x, y)
         dJdWList = np.concatenate([alldJdW[0].ravel(), alldJdW[1].ravel()])
-        for i in range(2,len(alldJdW)):
+        for i in range(2, len(alldJdW)):
             dJdWList = np.concatenate([dJdWList, alldJdW[i].ravel()])
         return dJdWList
 
@@ -133,88 +136,113 @@ class ANN(object):
                 json.dump(weight_list, codecs.open(filepath + str(i) + ".json", 'w', encoding='utf8'), sort_keys=True, indent = 4)
 
 
-
 """
 Neural_Network Check
 """
-class Network_Checker():
-    def compute_numerical_gradient(self,N,x,y):
-        paramsInitial = N.get_params()
-        numgrad = np.zeros(paramsInitial.shape)
-        perturb = np.zeros(paramsInitial.shape)
-        e = 1e-4
-        for p in range(len(paramsInitial)):
-            perturb[p]=e
-            N.set_params(paramsInitial+perturb)
-            loss2 = N.loss_function(x,y)
-            N.set_params(paramsInitial-perturb)
-            loss1 = N.loss_function(x,y)
-            numgrad[p] = (loss2-loss1)/(2*e)
-            perturb[p]=0
 
-        N.set_params(paramsInitial)
+
+class NetworkChecker(object):
+    def __init__(self):
+        pass
+
+    def compute_numerical_gradient(self, nn, x, y):
+        params_initial = nn.get_params()
+        numgrad = np.zeros(params_initial.shape)
+        perturb = np.zeros(params_initial.shape)
+        e = 1e-4
+        for p in range(len(params_initial)):
+            perturb[p] = e
+            nn.set_params(params_initial + perturb)
+            loss2 = nn.loss_function(x, y)
+            nn.set_params(params_initial - perturb)
+            loss1 = nn.loss_function(x, y)
+            numgrad[p] = (loss2 - loss1)/(2 * e)
+            perturb[p] = 0
+
+        nn.set_params(params_initial)
         return numgrad
 
-    def check(self,N,x,y):
-        numgrad = self.compute_numerical_gradient(N,x,y)
-        grad = N.compute_gradients(x,y)
-        if(len(grad) != len(numgrad)):
+    def check(self, nn, x, y):
+        numgrad = self.compute_numerical_gradient(nn, x, y)
+        grad = nn.compute_gradients(x, y)
+        if len(grad) != len(numgrad):
             print("Error! grad and numgrad have different sizes.")
             return False
         for i in range(len(grad)):
             difference = grad[i]-numgrad[i]
-            if(difference >0.0001 or difference < -0.0001):
+            if difference > 0.0001 or difference < -0.0001:
                 print ("Error! gradient and numerical gradient are different.")
                 return False
         print("Passed tests.")
         return True
 
+
 """
 Normalization of Data Functions
 """
-def norm_max(x, max=None):
-    if max:
-        return x/max
+
+
+def norm_max(x, max_val=None):
+    if max_val:
+        return x/max_val
     else:
-        return x/np.amax(x, axis = 0)
+        return x/np.amax(x, axis=0)
+
+
 def norm_percent(x):
     return x/100
+
+
 def norm_standard(x):
-    return (x - np.amin(x, axis = 0))/(np.amax(x, axis = 0) - np.amin(x, axis = 0))
+    return (x - np.amin(x, axis=0))/(np.amax(x, axis=0) - np.amin(x, axis=0))
+
+
 def shuffle_x_and_y(x, y):
     p = np.random.permutation(len(x))
     return x[p], y[p]
 
+
 def main():
-    lsizes = np.array(([784],[30],[30],[30],[10]))
-    learning_rate = 0.2
-    bias_learning_rate = 0.2
-    n = ANN(lsizes, squared_error, learning_rate, bias_learning_rate, momentum = 0.1, read_weights_from_file=False)
+    lsizes = np.array(([784], [30], [30], [10]))
+    learning_rate = 0.4
+    bias_learning_rate = 0.4
+    n = ANN(lsizes, squared_error, learning_rate, bias_learning_rate, read_weights_from_file=False)
     training_data, validation_data, testing_data = mnist_loader.load_data_wrapper()
 
     total_cost = 0
-    for i in range(len(training_data)):
-        cost = n.back_prop(training_data[i][0], training_data[i][1].T.ravel())
-        total_cost += cost
-        if i % 1000  == 0:
-            avg = float(total_cost)/1000
-            print "data #", i,"cost:", avg
-            total_cost = 0
+    for z in range(1):
+        for i in range(len(training_data)):
+            cost = n.back_prop(training_data[i][0], training_data[i][1].T.ravel())
+            total_cost += cost
+            if i % 1000 == 0:
+                avg = float(total_cost)/1000
+                print "data #", i,"cost:", avg
+                total_cost = 0
 
-    wrong = np.zeros(10)
+    wrong = np.zeros(10, dtype = np.int)
+    number_correct = np.zeros(10, dtype = np.int)
     correct = 0
     total = 0
+    average_forward = 0.0
     for i in range(len(testing_data)):
+        start = time.time()
         z = n.forward(testing_data[i][0])
+        end = time.time()
+        average_forward += (end - start)
         if np.argmax(z) == testing_data[i][1]:
             correct += 1
+            number_correct[testing_data[i][1]] += 1
         else:
             wrong[testing_data[i][1]] += 1
         total += 1
     percentage = float(correct)/total * 100
+    average_forward /= total
+
+    print "Average testing forward time: {0} ".format(str(average_forward))
     print percentage, "%"
-    print wrong
-    #n.save_weights(percentage, lsizes, learning_rate)
+    print "Correct: ", number_correct
+    print "Wrong:   ", wrong
+    # n.save_weights(percentage, lsizes, learning_rate)
 
 if __name__ == "__main__":
     main()
