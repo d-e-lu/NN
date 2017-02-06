0f73eb7fbf2fc6f883ae885b01198f828e422150
Dylan <dyl055a@gmail.com>
2017-02-05 23:34:06 -0800

    added example

---
+++ src/ANN/example.py
@@ -0,0 +1,21 @@
+import mnist_loader
+import numpy as np
+import ANN
+
+def main():
+    lsizes = np.array(([784], [30], [30], [10]))
+    learning_rate = 0.4
+    bias_learning_rate = 0.4
+    n = ANN.ArtificialNeuralNet(lsizes, ANN.squared_error, learning_rate, bias_learning_rate, read_weights_from_file=False)
+    training_data, validation_data, testing_data = mnist_loader.load_data_wrapper()
+
+    n.train(training_data)
+    percentage, correct, incorrect = n.test(testing_data=testing_data, output_size=training_data[0][1].size)
+
+    print percentage, "%"
+    print correct
+    print incorrect
+    # n.save_weights(percentage, lsizes, learning_rate)
+
+if __name__ == "__main__":
+    main()
